73d0c101a2de463ef6213fb24b2051bcaf263c9e
Igor Wendling <igor.w.silva@edu.ufes.br>
2025-12-07 16:53:18 -0300

    Primeiro commit

---
+++ src/main/java/com/mycompany/usercontrol/UserControl.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.usercontrol;
+
+/**
+ *
+ * @author igor2
+ */
+public class UserControl {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
